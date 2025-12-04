5cf49c023931e472257914aad64d6cca9307adf4
sarangbaraskar <sarangbaraskar07@gmail.com>
2025-12-04 17:57:01 +0530

    store issue solve

--- src/App/store.js
+++ src/App/store.js
@@ -2,5 +2,5 @@ import { configureStore } from "@reduxjs/toolkit";
 import { to_doSlice } from "../features/tod/To_doSlice";
 
 export const store = configureStore({
-    reducer: todoSlice
+    reducer: to_doSlice
 })
