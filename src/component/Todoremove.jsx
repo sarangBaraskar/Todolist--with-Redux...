d6791125b143a47345bf754a741c9feafecc195f
sarangbaraskar <sarangbaraskar07@gmail.com>
2025-12-04 17:22:40 +0530

    Path and code correction

--- src/component/Todoremove.jsx
+++ src/component/Todoremove.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { removetodo } from '../features/todo/Todoslice'
+import { removetodo } from '../features/tod/Todoslice'
 
 
 export default function Todoremove() {
